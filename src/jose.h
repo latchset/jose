f19278ccc290fb9b0a289ba555a7a05efece5e78
Nathaniel McCallum <npmccallum@redhat.com>
2016-06-10 10:55:10 -0400

    Add JWKSet functions

--- src/jose.h
+++ src/jose.h
@@ -63,3 +63,19 @@ jose_jwk_to_ec(const json_t *jwk, BN_CTX *ctx);
  */
 RSA *
 jose_jwk_to_rsa(const json_t *jwk, BN_CTX *ctx);
+
+/**
+ * Returns a copy of the JWKSet. Includes private key material.
+ *
+ * If the input is an array of JWKs, it is converted to a JWKSet.
+ */
+json_t *
+jose_jwkset_private(const json_t *jwkset);
+
+/**
+ * Returns a copy of the JWKSet. Excludes private key material.
+ *
+ * If the input is an array of JWKs, it is converted to a JWKSet.
+ */
+json_t *
+jose_jwkset_public(const json_t *jwkset);
---
+++ src/jwkset.c
@@ -0,0 +1,62 @@
+/* vim: set tabstop=8 shiftwidth=4 softtabstop=4 expandtab smarttab colorcolumn=80: */
+
+#include "jose.h"
+
+static const char *jwkprv[] = {
+    "k", "d", "p", "q", "dp", "dq", "qi", "oth", NULL
+};
+
+json_t *
+jose_jwkset_private(const json_t *jwkset)
+{
+    json_t *copy = NULL;
+
+    copy = json_deep_copy(jwkset);
+
+    if (json_is_array(copy))
+        copy = json_pack("{s:o}", "keys", copy);
+
+    if (!json_is_object(copy))
+        goto error;
+
+    return copy;
+
+error:
+    json_decref(copy);
+    return NULL;
+}
+
+json_t *
+jose_jwkset_public(const json_t *jwkset)
+{
+    const json_t *array = NULL;
+    json_t *copy = NULL;
+
+    copy = jose_jwkset_private(jwkset);
+    if (!copy)
+        return NULL;
+
+    array = json_object_get(copy, "keys");
+    if (!json_is_array(array))
+        goto error;
+
+    for (size_t i = 0; i < json_array_size(array); i++) {
+        json_t *jwk = json_array_get(array, i);
+
+        if (!json_is_object(jwk))
+            continue;
+
+        for (size_t j = 0; jwkprv[j]; j++) {
+            if (json_object_get(jwk, jwkprv[j]) &&
+                json_object_del(jwk, jwkprv[j]) == -1)
+                goto error;
+        }
+    }
+
+    return copy;
+
+error:
+    json_decref(copy);
+    return NULL;
+}
+
