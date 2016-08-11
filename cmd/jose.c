4642f3c78e8a9fa8bd176f7aece560689f7cedbf
Nathaniel McCallum <npmccallum@redhat.com>
2016-08-11 17:33:12 -0400

    License under Apache 2.0

--- cmd/jose.c
+++ cmd/jose.c
@@ -1,4 +1,19 @@
 /* vim: set tabstop=8 shiftwidth=4 softtabstop=4 expandtab smarttab colorcolumn=80: */
+/*
+ * Copyright 2016 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <cmd/jose.h>
 
@@ -207,6 +222,7 @@ main(int argc, char *argv[])
         { "ver", jcmd_ver },
         { "enc", jcmd_enc },
         { "dec", jcmd_dec },
+        { "hdr", jcmd_hdr },
         {}
     };
 
@@ -238,6 +254,7 @@ main(int argc, char *argv[])
 "\njose " VER_USE
 "\njose " ENC_USE
 "\njose " DEC_USE
+"\njose " HDR_USE
 "\n");
     return EXIT_FAILURE;
 }
--- lib/misc.c
+++ lib/misc.c
@@ -1,8 +1,24 @@
 /* vim: set tabstop=8 shiftwidth=4 softtabstop=4 expandtab smarttab colorcolumn=80: */
+/*
+ * Copyright 2016 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "misc.h"
 #include <jose/b64.h>
 
+#include <ctype.h>
 #include <string.h>
 
 json_t *
@@ -29,7 +45,9 @@ compact_to_obj(const char *compact, ...)
     memset(len, 0, sizeof(len));
 
     for (size_t i = 0; compact[i]; i++) {
-        if (compact[i] != '.')
+        if (!isalnum(compact[i]) && !strchr("-_.", compact[i]))
+            return NULL;
+        else if (compact[i] != '.')
             len[c]++;
         else if (++c > count - 1)
             return NULL;
