f35c7ed09a54d52c6d6c5723dabb3f66842bba13
Nathaniel McCallum <npmccallum@redhat.com>
2016-08-11 17:31:32 -0400

    Add a header merging command

---
+++ cmd/hdr.c
@@ -0,0 +1,112 @@
+/* vim: set tabstop=8 shiftwidth=4 softtabstop=4 expandtab smarttab colorcolumn=80: */
+
+#include <cmd/jose.h>
+#include <string.h>
+
+static const struct option opts[] = {
+    { "help",   no_argument,       .val = 'h' },
+
+    { "input",  required_argument, .val = 'i' },
+    { "number", required_argument, .val = 'n' },
+    { "output", required_argument, .val = 'o' },
+    {}
+};
+
+int
+jcmd_hdr(int argc, char *argv[])
+{
+    const json_t *rcps = NULL;
+    const json_t *sigs = NULL;
+    const json_t *shrd = NULL;
+    int ret = EXIT_FAILURE;
+    const char *sig = NULL;
+    const char *out = "-";
+    json_t *jose = NULL;
+    json_t *hdr = NULL;
+    char *in = NULL;
+    size_t len = 0;
+    size_t n = 0;
+
+    for (int c; (c = getopt_long(argc, argv, "hi:o:", opts, NULL)) >= 0; ) {
+        switch (c) {
+        case 'h': goto usage;
+        case 'o': out = optarg; break;
+        case 'n': n = strtoull(optarg, NULL, 10); break;
+        case 'i':
+            free(in);
+            in = jcmd_load_data(optarg, &len);
+            break;
+        default:
+            fprintf(stderr, "Invalid option: %c!\n", c);
+            free(in);
+            goto usage;
+        }
+    }
+
+    if (!in) {
+        fprintf(stderr, "Error reading input!\n");
+        goto usage;
+    }
+
+    jose = jose_jwe_from_compact(in);
+    if (!jose) {
+        jose = jose_jws_from_compact(in);
+        if (!jose)
+            jose = json_loadb(in, len, 0, NULL);
+    }
+
+    free(in);
+    if (!jose) {
+        fprintf(stderr, "Invalid JWS or JWE!\n");
+        goto egress;
+    }
+
+    if (json_unpack(jose, "{s?o,s?o,s?o,s?s}",
+                    "recipients", &rcps, "header", &shrd,
+                    "signatures", &sigs, "signature", &sig) != 0) {
+        fprintf(stderr, "Error unpacking JWS or JWE!\n");
+        goto egress;
+    }
+
+    if (json_is_array(rcps)) {
+        hdr = jose_jwe_merge_header(jose, json_array_get(rcps, n));
+    } else if (json_is_array(sigs)) {
+        hdr = jose_jws_merge_header(json_array_get(sigs, n));
+    } else if (json_is_object(shrd)) {
+        hdr = jose_jwe_merge_header(jose, jose);
+    } else if (sig) {
+        hdr = jose_jws_merge_header(jose);
+    } else {
+        hdr = jose_jwe_merge_header(jose, NULL);
+    }
+
+    if (!hdr) {
+        fprintf(stderr, "Error merging headers!\n");
+        goto egress;
+    }
+
+    jcmd_dump_json(hdr, out, NULL);
+
+egress:
+    json_decref(jose);
+    json_decref(hdr);
+    return ret;
+
+usage:
+    fprintf(stderr,
+"jose " HDR_USE
+"\n"
+"\nExtracts the JOSE Header."
+"\n"
+"\nNOTE WELL: This command does not validate the protected header!"
+"\n"
+"\n    -i FILE,   --jwk=FILE       JWS or JWE input (file)"
+"\n    -i -,      --jwk=-          JWS or JWE input (stdin)"
+"\n"
+"\n    -n NUM,    --number=NUM     Recipient or signature number (default: 0)"
+"\n"
+"\n    -o FILE,   --output=FILE    JOSE Header output (file)"
+"\n    -o -,      --output=-       JOSE Header output (stdout; default)"
+"\n\n");
+    goto egress;
+}
